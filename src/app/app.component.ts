eb3b03ead5fcb8b4f55b1896d4285da96f41df0b
Artur Zabeyvorota <artur.zabeyvorota@ruter.no>
2019-05-24 22:55:58 +0200

    elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+interface ElementInfo {
+  name: string;
+  meta?: Map<string, string>[];
+}
+
+interface DetailState {
+  progress: number;
+  elements: ElementInfo[];
+}
+
 interface Detail {
   id?: number;
   name: string;
@@ -8,32 +18,45 @@ interface Detail {
   created: Date;
   updated: Date;
   addition: string;
+  state: DetailState;
 }
 
 const DETAILS: Detail[] = [
   {
     name: 'Деталь 1',
     type: 'Тип 1',
-    status: '2/30',
+    status: 'В разработке',
     created: new Date('1995-12-17T03:24:00'),
     addition: '-',
-    updated: new Date('1995-12-17T03:24:00')
+    updated: new Date('1995-12-17T03:24:00'),
+    state: {
+      elements: [{name: 'Шаг 1'}, {name: 'Шаг 2'}, {name: 'Шаг 3'}, {name: 'Шаг 4'}, {name: 'Шаг 5'}],
+      progress: 1
+    }
   },
   {
     name: 'Деталь 2',
-    status: '2/30',
     type: 'Тип 2',
+    status: 'В разработке',
     created: new Date('1995-12-17T03:24:00'),
     addition: '-',
-    updated: new Date('1995-12-17T03:24:00')
+    updated: new Date('1995-12-17T03:24:00'),
+    state: {
+      elements: [{name: 'Шаг 1'}, {name: 'Шаг 2'}, {name: 'Шаг 3'}, {name: 'Шаг 4'}, {name: 'Шаг 5'}],
+      progress: 3
+    }
   },
   {
     name: 'Деталь 3',
     type: 'Тип 3',
-    status: '2/30',
+    status: 'В разработке',
     created: new Date('1995-12-17T03:24:00'),
     addition: '-',
-    updated: new Date('1995-12-17T03:24:00')
+    updated: new Date('1995-12-17T03:24:00'),
+    state: {
+      elements: [{name: 'Шаг 1'}, {name: 'Шаг 2'}, {name: 'Шаг 3'}, {name: 'Шаг 4'}, {name: 'Шаг 5'}],
+      progress: 5
+    }
   }
 ];
 
@@ -44,7 +67,7 @@ const DETAILS: Detail[] = [
 })
 export class AppComponent {
 
-  activeDetail:Detail;
+  activeDetail: Detail;
 
   detailTypes = DETAILS.map(a => a.type );
 
@@ -58,7 +81,13 @@ export class AppComponent {
       .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 
-  changeActiveDetail(detail) {
+  changeActiveDetail(detail: Detail) {
     this.activeDetail = detail;
+  }
+
+  generateStatus(detail: Detail): string {
+    const current = detail.state.progress + 1;
+    const total = detail.state.elements.length;
+    return `${detail.status} [${current}/${total}]`;
   }
 }
